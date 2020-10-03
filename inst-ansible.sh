3cac411a8b67a58b79fe7a9c2ebae29171d1f194
fao4000 <fao4000@mail.ru>
2020-10-03 13:23:24 +0700

    first commit

---
+++ inst-ansible.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# Updates
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt autoremove -y
+
+# Install Git
+sudo apt install git -y
+
+# Install VBox GuestAdd
+sudo apt-get install virtualbox-guest-additions-iso -y
+
+# Install Ansible
+sudo apt install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt update -y
+sudo apt install ansible -y
+
